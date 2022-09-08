14f36f35c61d3bfa3172f3fdaf2899b8c58fd413
Thế Hào <thehao159@gmail.com>
2022-09-08 10:06:10 +0700

    Add Tablet Animation Main Slider
    - Add Tablet Animation Main Slider.

--- assets/js/common-slider.js
+++ assets/js/common-slider.js
@@ -8,6 +8,7 @@ function handleChangeIndex(currentIndex, newIndex, btnsClassName, parentClassNam
     else {
         document.querySelector(parentClassName).style.transform = `translateX(calc((-100% / 6) * ${newIndex})`;
     }
+    document.querySelector(parentClassName).style.transition = `transform 0.5s ease`;
     btnElements[currentIndex].classList.remove('content__slider-btn--selected');
     btnElements[newIndex].classList.add('content__slider-btn--selected');
     currentIndex = newIndex;
@@ -34,5 +35,22 @@ function checkLimitPre(currentIndex, btnsClassName) {
     }
     else {
         return btnElements.length - 1;
+    }
+}
+
+function handleMoveSLider(range, currentIndex, sliderClassName, btnsClassName) {
+    var btnElements = document.querySelectorAll(btnsClassName);
+
+    if(currentIndex === 0 && range < 0) return;
+    if(currentIndex === btnElements.length - 1 && range >= 0) return;
+    document.querySelector(sliderClassName).style.transform = `translateX(calc((-100% / 6) * ${currentIndex} - ${range}px)`;
+    document.querySelector(sliderClassName).style.transition = `transform 0.01s ease`;
+}
+
+function handleGestureSlider(currentIndex, btnsClassName) {
+    if (touchendX < touchStartX) {
+        return checkLimitNext(currentIndex, btnsClassName);
+    } else if (touchendX > touchStartX) {
+        return checkLimitPre(currentIndex, btnsClassName);
     }
 }
